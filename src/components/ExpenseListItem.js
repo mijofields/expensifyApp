2a54a31368dee3a7281fde33b0f94eeea6acf3f0
Mike Fields <mikefields81@gmail.com>
2019-03-11 12:03:27 -0500

    changes to ExpenseListItem to format date and ccy

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 
 const ExpenseListItem = ({ id, description, amount, createdAt}) => (
@@ -7,8 +9,12 @@ const ExpenseListItem = ({ id, description, amount, createdAt}) => (
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>Cost: {amount}, Date: {createdAt} </p>
-        <p>{id}</p>
+        <p>
+            Cost: {numeral(amount).format('$0,0.00')}
+        </p>
+        <p>            
+            Date: {moment(createdAt).format('MMM Do, YYYY')} 
+        </p>
     </div>
 );
 
