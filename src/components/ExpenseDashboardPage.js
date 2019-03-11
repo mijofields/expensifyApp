23d4b6837c415d6e46061bbb8c06652f61f74a85
Mike Fields <mikefields81@gmail.com>
2019-03-11 09:26:39 -0600

    production webpack build

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 
+
 const ExpenseDashboardPage = () => (
     <div>
         <ExpenseListFilters />
