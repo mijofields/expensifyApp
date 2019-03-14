7164938e1c57635226791c3fa4424d28adc10ac5
Mike Fields <mikefields81@gmail.com>
2019-03-13 19:15:35 -0500

    Adding PrivateRoutes and setting up auth actions / reducer

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter, { history } from './routers/AppRouter.js';
 import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter.js';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-// import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 // import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -45,7 +45,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log('uid', user.uid)
+        store.dispatch(login(user.uid));
         store.dispatch(startSetExpenses()).then(() => {
         renderApp();
         if (history.location.pathname === '/') {
@@ -53,6 +53,7 @@ firebase.auth().onAuthStateChanged((user) => {
         }
     });
     } else {
+       store.dispatch(logout());
         renderApp();
         history.push('/');
     }
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Header from '../components/Header.js';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import CreateExpensePage from '../components/CreateExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
-
+import PrivateRoute from './PrivateRoute';
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history}>
     <div>
-        <Header />
         <Switch>
             <Route path="/" component={LoginPage} exact={true} />
-            <Route path="/dashboard" component={ExpenseDashboardPage} />
-            <Route path="/create" component={CreateExpensePage} />
-            <Route path="/edit/:id" component={EditExpensePage} />
+            <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+            <PrivateRoute path="/create" component={CreateExpensePage} />
+            <PrivateRoute path="/edit/:id" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
             <Route component={NotFoundPage}/>
         </Switch>
